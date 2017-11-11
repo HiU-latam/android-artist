ebd7ed3c5f9784e688151779b2f1ef61ae09366e
shiny solutions <shinysolutions16@gmail.comdsd>
2017-11-11 07:43:39 +0530

    Screen # 3 Requests screen changes

---
+++ app/src/main/java/com/hiulatam/hiu/hiuartist/VideoActivity.java
@@ -0,0 +1,44 @@
+package com.hiulatam.hiu.hiuartist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by:  Shiny Solutions
+ * Created on:  11/10/17.
+ */
+
+public class VideoActivity extends AppCompatActivity {
+
+    private static final String TAG = "VideoActivity - ";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  11/10/17.
+     */
+    private void bindComponents(){
+
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  11/10/17.
+     */
+    private void init(){
+
+    }
+
+    /**
+     * Created by:  Shiny Solutions
+     * Created on:  11/10/17.
+     */
+    private void addListeners(){
+
+    }
+}
